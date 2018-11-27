80b7fdb3db5b64d9c4f8327dcb21a7c53cd5061f
Mac <39278228+Reithger@users.noreply.github.com>
2018-11-27 15:00:00 -0400

    Testing

--- src/testing/JUnitTest.java
+++ src/testing/JUnitTest.java
@@ -23,6 +23,28 @@ class JUnitTest {
 		assertEquals(stor.makePurchase(0, "Austria"), false);
 		assertEquals(stor.makePurchase(0, "Australia"), true);
 		
+		//Destination testing
+		Canada can = (Canada)DestinationFactory.getItemDestination("Canada");
+		assertEquals(can.getCountryName(), "Canada");
+		assertEquals(DestinationFactory.getItemDestination("Can"), null);
+		
+		//Lollipop testing
+		
+		Blinky blink = (Blinky)LollipopFactory.getLollipop("b");
+		assertEquals(blink.getFlavor(), "Blink-licious");
+		assertEquals(blink.getName(), "Blinky Lollipop");
+		assertEquals(blink.getId(), 2);
+		assertEquals(blink.getPrice(), 1.75);
+		assertEquals(LollipopFactory.getLollipop("s"), null);
+		
+		//MusicSlipper testing
+		
+		BoogieWonderland bW = (BoogieWonderland)MusicSlipperFactory.getMusicSlipper("b");
+		assertEquals(bW.getName(), "Musical Slipper");
+		assertEquals(bW.getSong(), "Dance, Boogie Wonderland");
+		assertEquals(bW.getId(), 3);
+		assertEquals(bW.getPrice(), 22.50);
+		assertEquals(MusicSlipperFactory.getMusicSlipper("les"), null);
 		
 	}
 
